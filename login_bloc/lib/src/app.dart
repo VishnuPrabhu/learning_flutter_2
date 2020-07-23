589f2380e9d3284e8c584884d15c864897a42cf8
Vishnu Prabhu <VishnuPrabhu.Ramachandran@uobgroup.com>
2020-07-24 00:16:07 +0800

    learning - stream builder intro and inherited scope

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'blocs/provider.dart';
 import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           SizedBox(height: 20),
           submitButton(),
         ],
@@ -16,23 +21,37 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'Enter you email address',
-        labelText: 'Email Address',
-      ),
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'Enter you email address',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+          onChanged: bloc.changeEmail,
+        );
+      },
     );
   }
 
-  Widget passwordField() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'enter atleast 4 digits password',
-        labelText: 'Password',
-      ),
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder<String>(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'enter atleast 4 digits password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+          onChanged: bloc.changePassword,
+        );
+      },
     );
   }
 
