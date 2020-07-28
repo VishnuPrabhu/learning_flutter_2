f6844a6b307e67d5fc78baac44c320999d49e5c3
Vishnu Prabhu <VishnuPrabhu.Ramachandran@uobgroup.com>
2020-07-29 01:37:55 +0800

    add submit button validation

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
+
 import 'validators.dart';
 
 class Bloc with Validators {
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   // Change data
   Function(String) get changeEmail => _emailController.sink.add;
@@ -13,6 +15,15 @@ class Bloc with Validators {
   // Add data to stream
   Stream<String> get email => _emailController.stream.transform(validateEmail);
   Stream<String> get password => _passwordController.stream.transform(validatePassword);
+  Stream<bool> get submitValid => Rx.combineLatest2(email, password, (e, p) => true);
+
+  void submit() {
+    final validEmail = _emailController.value;
+    final validPassword = _passwordController.value;
+
+    print('Email is $validEmail');
+    print('Password is $validPassword');
+  }
 
   void dispose() {
     _emailController.close();
--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -15,7 +15,7 @@ class LoginScreen extends StatelessWidget {
           emailField(bloc),
           passwordField(bloc),
           SizedBox(height: 20),
-          submitButton(),
+          submitButton(bloc),
         ],
       ),
     );
@@ -55,11 +55,20 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: snapshot.hasData
+              ? () {
+                  bloc.submit();
+                }
+              : null,
+        );
+      },
     );
   }
 }
