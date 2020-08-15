2799d47bb87ead8f7bb2f49b1ea245beb8d66774
Vishnu Prabhu <VishnuPrabhu.Ramachandran@uobgroup.com>
2020-08-15 12:11:17 +0800

    initial commit of animation 1. AnimationController -> start() and stop() 2. Animation -> has parameter {parent: Controller} 3. Tween -> range 4. AnimatedBuider - pass the widget & animation and use the value 4. AnimationStatusListener - listen to the changes

--- animation_app/lib/main.dart
+++ animation_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'src/app.dart';
 
 void main() {
+  debugPaintSizeEnabled = true; // draws border around the widgets to find layout in the screen
   runApp(App());
 }
